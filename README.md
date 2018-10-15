31bde090f46601371f3277942ded6c9628d89686
Nicholas Rempel <nbrempel@gmail.com>
2018-10-15 16:55:30 -0700

    Hello, World
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

---
+++ README.md
@@ -0,0 +1,17 @@
+# VON Personal Agent
+
+A HL Indy agent for individuals to hold credentials
+
+## Running
+
+```
+cd docker
+```
+
+```
+docker-compose build
+```
+
+```
+docker-compose up
+```
---
+++ src/server.py
@@ -0,0 +1,26 @@
+import json
+import os
+import logging
+
+from aiohttp import web
+
+
+logging.getLogger()
+LOGGER = logging.getLogger(__name__)
+
+PORT = os.environ.get("PORT")
+
+APP = web.Application()
+ROUTES = web.RouteTableDef()
+
+
+@ROUTES.get("/")
+async def index(request):
+    return web.FileResponse("public/index.html")
+
+
+if __name__ == "__main__":
+    APP.add_routes(ROUTES)
+    LOGGER.info("Running webserver...")
+    web.run_app(APP, host="0.0.0.0", port=PORT)
+
