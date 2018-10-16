a6502f19bbe6b060fc9e95304257b1c5b9b20e23
Nicholas Rempel <nbrempel@gmail.com>
2018-10-16 11:55:10 -0700

    Add basic QR example
    Signed-off-by: Nicholas Rempel <nbrempel@gmail.com>

--- src/app/src/app/scanner/scanner.module.ts
+++ src/app/src/app/scanner/scanner.module.ts
@@ -5,11 +5,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ScannerPage } from './scanner.page';
 
+import { ZXingScannerModule } from '@zxing/ngx-scanner';
+
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    ZXingScannerModule,
     RouterModule.forChild([{ path: '', component: ScannerPage }])
   ],
   declarations: [ScannerPage],
--- src/app/src/app/scanner/scanner.page.ts
+++ src/app/src/app/scanner/scanner.page.ts
@@ -1,4 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
+
+import { ZXingScannerComponent } from "@zxing/ngx-scanner";
+import { Result } from "@zxing/library";
 
 @Component({
   selector: "app-scanner",
@@ -6,7 +9,46 @@ import { Component } from "@angular/core";
   styleUrls: ["scanner.page.scss"]
 })
 export class ScannerPage {
-  constructor() {
-    
+  @ViewChild("scanner")
+  scanner: ZXingScannerComponent;
+
+  SUPPORTED_FORMATED = ["QR_CODE"];
+
+  hasDevices: boolean;
+  hasPermission: boolean;
+  qrResultString: string;
+  qrResult: Result;
+
+  availableDevices: MediaDeviceInfo[];
+  device: MediaDeviceInfo;
+
+  constructor() {}
+
+  ionViewDidEnter() {
+    this.scanner.camerasFound.subscribe((devices: MediaDeviceInfo[]) => {
+      this.hasDevices = true;
+      this.availableDevices = devices;
+
+      // selects the devices's back camera by default
+      for (const device of devices) {
+        if (/back|rear|environment/gi.test(device.label)) {
+          this.scanner.changeDevice(device);
+          this.device = device;
+          break;
+        }
+      }
+    });
+    this.scanner.camerasNotFound.subscribe(() => (this.hasDevices = false));
+    this.scanner.scanComplete.subscribe(
+      (result: Result) => (this.qrResult = result)
+    );
+    this.scanner.permissionResponse.subscribe(
+      (perm: boolean) => (this.hasPermission = perm)
+    );
+  }
+
+  handleQrCodeResult(resultString: string) {
+    console.debug("Result: ", resultString);
+    this.qrResultString = resultString;
   }
 }
